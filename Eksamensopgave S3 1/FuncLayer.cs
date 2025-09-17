befccc6ff4d8bc45703ca8e40cd34726aac6b19e
Sebastian Stahlhut Wøhlk <66186@edu.campusvejle.dk>
2025-09-17 15:10:16 +0200

    awdasdawdada

--- Eksamensopgave S3 1/FuncLayer.cs
+++ Eksamensopgave S3 1/FuncLayer.cs
@@ -62,41 +62,31 @@ namespace Eksamensopgave_S3_1
         public Bog TilføjBog(string Forfatter, string Titel, string Udgiver,
             DateTime? date, int AntalEksemplarer, string ISBN)
         {
-            // Check if any of the required book details are missing or invalid
             ValiderBogData(Forfatter, Titel, Udgiver, date, AntalEksemplarer, ISBN);
 
-            // Create a new 'Bog' (Book) object using the provided details
             Bog bog = new Bog(
-                0, // Assuming Entity Framework will assign a unique ID
+                0,
                 Forfatter,
                 Titel,
                 Udgiver,
-                date.Value, // Create a new DateOnly object from the publication year/month/day
+                date.Value,
                 AntalEksemplarer,
                 ISBN
             );
 
-            // Add the newly created book object to the 'BogTabel' (BookTable) in the database context
             model.BogTabel.Add(bog);
-            // Save the changes to the database
             model.SaveChanges();
-            // Notify the UI or other parts of the application that the 'BogListe' (BookList) has changed
             RaisePropertyChanged(nameof(BogListe));
-            // Return the newly added book object
             return bog;
         }
 
         public Bog RedigereBog(string Forfatter, string Titel, string Udgiver,
             DateTime? date, int AntalEksemplarer, string ISBN)
         {
-            // Validate the input arguments to ensure none are null, empty, or less than 1.
-            // An ArgumentException is thrown if any of the provided values are invalid.
             ValiderBogData(Forfatter, Titel, Udgiver, date, AntalEksemplarer, ISBN);
 
-            // Find the book in the database table ('BogTabel') that matches the given ISBN.
             var bog = model.BogTabel.FirstOrDefault(b => b.ISBN == ISBN);
 
-            // If no book is found with the specified ISBN, throw an InvalidOperationException.
             if (bog == null)
             {
                 throw new InvalidOperationException("Bogen med det angivne ISBN blev ikke fundet.");
@@ -105,7 +95,6 @@ namespace Eksamensopgave_S3_1
             var existingBog = BogListe.First(v => v.ISBN == ISBN);
             if (existingBog != null)
             {
-                // Update the properties of the found book object with the new values provided as arguments.
                 bog.Forfatter = Forfatter;
                 PropertyChanged?.Invoke(existingBog, new PropertyChangedEventArgs(nameof(existingBog.Forfatter)));
                 bog.Titel = Titel;
@@ -122,11 +111,8 @@ namespace Eksamensopgave_S3_1
                 throw new Exception("bog Eksisterer ikke");
             }
 
-            // Save the changes made to the book object back to the database.
             model.SaveChanges();
-            // Notify the UI or other parts of the application that the book list property has changed.
             RaisePropertyChanged(nameof(BogListe));
-            // Return the updated book object.
             return bog;
         }
 
@@ -152,7 +138,6 @@ namespace Eksamensopgave_S3_1
 
             ValiderUdlånBogData(date, Låner, AntalBøger);
 
-            // Create a new 'Bog' (Book) object using the provided details
             UdlånBog udlånBog = new UdlånBog(
                 0,
                 date.Value,
@@ -162,7 +147,6 @@ namespace Eksamensopgave_S3_1
             );
 
             model.Add(udlånBog);
-            //RedigereBog(bog.Forfatter, bog.Titel, bog.Titel, bog.UdgivelseDato.Day, bog.UdgivelseDato.Month, bog.UdgivelseDato.Year, bog.AntalEksemplarer - AntalBøger, bog.ISBN);
             RaisePropertyChanged(nameof(UdlånBogListe));
 
             return udlånBog;
@@ -172,7 +156,6 @@ namespace Eksamensopgave_S3_1
         {
             ValiderUdlånBogData(date, låner, antalBøger);
 
-            // Find the book in the database table ('BogTabel') that matches the given ISBN.
             var existingUdlånBog = model.UdlånBogTabel.FirstOrDefault(b => b.Bog.ISBN == ISBN);
             if (existingUdlånBog != null)
             {
